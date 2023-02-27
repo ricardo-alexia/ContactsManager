12c5ff4075504f3d75097e404036b9e5310619e3
Alexia Briones Ornelas <brionesornelas.alexia@gmail.com>
2023-02-27 12:13:25 -0600

    finished!!! :)

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -4,23 +4,10 @@ import java.util.Map;
 
 public class ContactsApp{
 
-
-    private static void display(){
-        System.out.println("""
-                1. View contacts.
-                2. Add a new contact.
-                3. Search a contact by name.
-                4. Delete an existing contact.
-                5. Exit.
-                Enter an option (1, 2, 3, 4 or 5):
-                """);
-    }
-
     public static void main(String[] args) {
 
+        Contacts.userOptions();
 
-//      Contacts.addContacts();
-        Contacts.deleteContact();
     }
 
 
--- src/utils/Contacts.java
+++ src/utils/Contacts.java
@@ -1,10 +1,6 @@
 package utils;
 
-import javax.xml.crypto.dsig.spec.XPathType;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -20,20 +16,40 @@ public class Contacts {
 
     final static String filePath = "src/contacts.txt";
 
-   public static Path pathToFile = Paths.get(filePath);
+    public static Path pathToFile = Paths.get(filePath);
 
-   static Input userInput = new Input();
+    static Input userInput = new Input();
 
-      public static  Map<String, String> map
-                = new HashMap<String, String>();
+    static File file = new File(filePath);
+
+    public static Map<String, String> map
+            = new HashMap<String, String>();
+
+
+
+
+    ///// BASIC MENU //////
+    public static void display(){
+        Contacts.HashMapFromTextFile();
+        System.out.print("""
+                1. View contacts.
+                2. Add a new contact.
+                3. Search a contact by name.
+                4. Delete an existing contact.
+                5. Exit.
+                Enter an option (1, 2, 3, 4 or 5):
+                """);
+    }
+
+
+
+    ///// METHOD THAT READS THE TEXT FILE AND ADDS TO THE HASH MAP//////
+
     public static Map<String, String> HashMapFromTextFile() {
 
         BufferedReader br = null;
 
         try {
-
-            // create file object
-            File file = new File(filePath);
 
             // create BufferedReader object from the File
             br = new BufferedReader(new FileReader(file));
@@ -55,39 +71,44 @@ public class Contacts {
                 if (!name.equals("") && !number.equals(""))
                     map.put(name, number);
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
 
             // Always close the BufferedReader
             if (br != null) {
                 try {
                     br.close();
+                } catch (Exception e) {
                 }
-                catch (Exception e) {
-                };
+                ;
             }
         }
 
         return map;
     }
 
+
+    /// METHOD THAT PRINTS THE HASH MAP TO THE CONSOLE/////
     public static void print() {
 
         Map<String, String> mapFromFile
                 = Contacts.HashMapFromTextFile();
 
         System.out.println("""
+    
                 Name | Phone Number
                 -------------------""");
         for (Map.Entry<String, String> entry :
                 mapFromFile.entrySet()) {
             System.out.println(entry.getKey() + " | "
-                    + entry.getValue());
+                    + entry.getValue().replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"));   //(123) 456-7890);
         }
+        System.out.println();
     }
+
+
+    //// METHOD TO ADD A CONTACT TO THE TEXT FILE AND HASH MAP ///////
 
     public static void addContacts() {
         System.out.println("Please enter a name for your new contact:");
@@ -96,38 +117,101 @@ public class Contacts {
         String number = userInput.getString();
 
         try {
-            Files.writeString(pathToFile, String.format("%s:%s", name, number), StandardOpenOption.APPEND);
-        } catch (IOException iox){
+            Files.writeString(pathToFile, String.format("%s:%s%n", name, number), StandardOpenOption.APPEND);
+        } catch (IOException iox) {
             iox.printStackTrace();
         }
-
         print();
     }
 
+    //// METHOD TO DELETE A CONTACT TO THE TEXT FILE AND HASH MAP ///////
     public static void deleteContact() {
         print();
         System.out.println("Please enter the contact you'd like to delete:");
         String name = userInput.getString();
 
-//        List<String> currentList = new ArrayList<>();
-//        try {
-//            currentList = Files.readAllLines(pathToFile);
-//        } catch (IOException iox){
-//            iox.printStackTrace();
-//        }
+        map.remove(name);
 
-//        map.remove();
+        BufferedWriter bf = null;
+        try {
+            // create new BufferedWriter for the output file
+            bf = new BufferedWriter(new FileWriter(file));
 
-//        print();
+            // iterate map entries
+            for (Map.Entry<String, String> entry :
+                    map.entrySet()) {
 
-//        currentList.removeIf(item -> item.equals(name));
-//        try {
-//            Files.write(pathToFile, currentList);
-//        } catch (IOException iox) {
-//            iox.printStackTrace();
-//        }
+                // put key and value separated by a colon
+                bf.write(entry.getKey() + ":"
+                        + entry.getValue());
+
+                // new line
+                bf.newLine();
+            }
+
+            bf.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+
+            try {
+
+                // always close the writer
+                bf.close();
+            }
+            catch (Exception e) {
+            }
+        }
+
+        print();
+
+    }
+
+    //// METHOD TO SEARCH FOR A CONTACT IN THE HASH MAP AND PRINT OUT THAT CONTACT///////
+    public static void searchContact()
+    {
+        System.out.println("Please enter the name of the contact you are searching for: ");
+        String name = userInput.getString();
+        map.get(name);
+
+        System.out.printf("%s | %s%n%n", name, map.get(name));
     }
 
 
+    ////// METHOD THAT PULLS ALL THE OTHER METHODS TOGETHER ///////
+    public static void userOptions()
+    {
+        display();
+        String numberIn = userInput.getString();
+        switch(Integer.parseInt(numberIn))
+        {
+            case 1:
+                print();
+                userOptions();
+                break;
+            case 2:
+                addContacts();
+                userOptions();
+                break;
+            case 3:
+                searchContact();
+                userOptions();
+                break;
+            case 4:
+                deleteContact();
+                userOptions();
+                break;
+            case 5:
+                System.out.println("");
+                break;
+            default:
+                System.out.println("Please enter a valid option...");
+                userOptions();
+        }
+    }
+
 }
+
 
